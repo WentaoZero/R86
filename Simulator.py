e48ff7a563b51adefee426b9b167149188b8423f
Rahn <zaffrahn@gmail.com>
2016-03-29 21:50:17 +0800

    read file name from command line

--- Simulator.py
+++ Simulator.py
@@ -1,12 +1,14 @@
 from Parser import yacc
 from Parser import R86Processor
+import argparse
+
 
 class Simulator:
 	def setMemory(self):
 		for i in range(0, 10):
 			R86Processor.setMemory(0, i*4)
-	def readFile(self):
-		with open("Simple.asm") as InputFile:
+	def readFile(self, vFileName):
+		with open(vFileName) as InputFile:
 			for line in InputFile:
 				CleanLine = line.strip("\n\t")
 				if (0 != len(CleanLine) and CleanLine[0] != ";"):
@@ -18,7 +20,15 @@ class Simulator:
 	def printMemory(self):
 		R86Processor.printMemory()
 
-MySim = Simulator()
-MySim.setMemory()
-MySim.readFile()
-R86Processor.printSelf()
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("AsmFile", type=str, help="Assembly File")
+	args = parser.parse_args()
+
+	MySim = Simulator()
+	MySim.setMemory()
+
+	MySim.readFile(args.AsmFile)
+	R86Processor.printSelf()
+
